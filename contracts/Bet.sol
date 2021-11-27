f78717523fb4398615a67b75a6b4211959c58135
Alessia Lin <81080301+alessialin@users.noreply.github.com>
2021-11-27 17:43:55 +0100

    Update Bet.sol

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,1 +1,49 @@
 pragma solidity >=0.5.0 <0.6.0;
+
+contract Bet {
+    
+
+    uint buyerMaxAmount;
+    uint buyerAmountLeft;
+    address buyerAddress;
+    
+    uint eventId;
+    uint8 betScenario;
+    uint odds;
+    uint sellerMaxAmount;
+    uint sellerAmountLeft;
+
+    address[] public buyers;
+    address[] public sellers;
+    mapping(address => uint256) public balanceOfSeller;
+    mapping(address => uint256) public balanceOfBuyer;
+
+
+
+
+    constructor(uint _eventId, uint8 _betScenario, uint _odds, uint _sellerMaxAmount) public {
+
+        eventId = _eventId;
+        betScenario = _betScenario;
+        odds = _odds;
+        sellerMaxAmount = _sellerMaxAmount;
+        sellerAmountLeft = _sellerMaxAmount;
+        buyerMaxAmount = _sellerMaxAmount/ _odds;
+        buyerAmountLeft = buyerMaxAmount;
+    }
+
+    function placeStakeSeller(uint _amount) public payable {
+        require(msg.value == _amount);
+        require(_amount <= sellerAmountLeft);
+        balanceOfSeller[msg.sender] += _amount;
+        sellers.push(msg.sender);
+        sellerAmountLeft -= _amount;
+    }
+
+    function placeStakeBuyer(uint _amount) public payable {
+        require(msg.value == _amount);
+        require(_amount <= buyerAmountLeft);
+        balanceOfBuyer[msg.sender] += _amount;
+        buyerAmountLeft -= _amount;
+    }
+
