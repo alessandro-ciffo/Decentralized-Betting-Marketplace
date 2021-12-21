5e6a97052331f0d20e6e2284fecf26e2b1880fee
Alessandro Ciffo <diogene.ciffo@gmail.com>
2021-12-21 22:00:58 +0100

    updated api

--- api/match_result_api.py
+++ api/match_result_api.py
@@ -15,30 +15,6 @@ def home():
     return '''<h1>Decentralized Betting Marketplace</h1>
 <p>The first place where you can buy and sell bets in a decentralized fashion.</p>'''
 
-# @app.route('/api/v1/matches/', methods=['GET'])
-# def api_id():
-#     # Check if an ID was provided as part of the URL.
-#     # If ID is provided, assign it to a variable.
-#     # If no ID is provided, display an error in the browser.
-#     if 'id' in request.args:
-#         id = int(request.args['id'])
-#     else:
-#         return "Error: No id field provided. Please specify an id."
-
-#     # Create an empty list for our results
-#     results = []
-
-#     # Loop through the data and match results that fit the requested ID.
-#     # IDs are unique, but other fields might return many results
-#     for match in matches:
-#         if match['id'] == id:
-#             results.append(match)
-#             break
-
-#     # Use the jsonify function from Flask to convert our list of
-#     # Python dictionaries to the JSON format.
-#     return jsonify(results[0])
-
 
 @app.route('/api/v1/matches/', methods=['GET'])
 def api_name():
@@ -59,8 +35,6 @@ def api_name():
     # Use the jsonify function from Flask to convert our list of
     # Python dictionaries to the JSON format.
     return jsonify(matches)
-
-
 
 
 if __name__ == "__main__":
